044dbae2d9cda4026e346256d99c33999baa3ad3
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2022-12-29 19:00:43 -1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setuptools.setup(
                       'requests',
                       'tqdm',
                       'ninja',
+                      'imageio==2.4.1',
                       'imageio-ffmpeg==0.4.3',
                       'pyspng==0.1.0']
 )
